be3e4174de51f319b9545fb88185c29faf40ad32
bohendo <bo.henderson@trailofbits.com>
2026-04-15 11:49:07 -0400

    set RCI mutant's severity to medium

--- src/languages/solidity/mutations.rs
+++ src/languages/solidity/mutations.rs
@@ -3,5 +3,5 @@ use crate::types::{Mutation, MutationSeverity};
 pub const SOLIDITY_MUTATIONS: &[Mutation] = &[Mutation {
     slug: "RCI",
     description: "Require Condition Inversion: Invert the condition in require/assert (condition -> !condition)",
-    severity: MutationSeverity::High,
+    severity: MutationSeverity::Medium,
 }];
